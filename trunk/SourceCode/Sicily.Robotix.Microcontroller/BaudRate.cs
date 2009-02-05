59de9e13d9fb8973c292e53ee1dd632d7c49e188
bryancostanich <bryancostanich@deb72ea4-f350-11dd-a295-0b7b0935bf94>
2009-02-05 07:27:24 +0000

    Initial Import

---
+++ trunk/SourceCode/Sicily.Robotix.Microcontroller/BaudRate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sicily.Robotix
+{
+	/// <summary>
+	/// A list of common baud rates
+	/// </summary>
+	public enum BaudRate : int
+	{
+		Baud300 = 300
+		, Baud1200 = 1200
+		, Baud2400 = 2400
+		, Baud4800 = 4800
+		, Baud9600 = 9600
+		, Baud14400 = 14400
+		, Baud19200 = 19200
+		, Baud28800 = 28800
+		, Baud38400 = 38400
+		, Baud57600 = 57600
+		, Baud115200 = 115200
+	}
+}
---
+++ trunk/SourceCode/Sicily.Robotix.Microcontroller/Robot.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sicily.Robotix.MicroController;
+using System.Reflection;
+
+namespace Sicily.Robotix
+{
+	//=========================================================================
+	public static class Robot
+	{
+		//=========================================================================
+		/// <summary>
+		/// Creates a new IRobot from the configuration specified.
+		/// </summary>
+		/// <param name="configuration"></param>
+		/// <returns></returns>
+		public static IRobot FromConfiguration(RobotConfiguration configuration)
+		{
+			//---- declare vars
+			IRobot robot = new RobotBase();
+			string loadMessage;
+
+			//---- if it has a custom class
+			if (configuration.HasCustomClass)
+			{
+				//---- check to see if the assembly exists
+				if (System.IO.File.Exists(configuration.RobotClassAssemblyPath))
+				{
+					//---- declare vars
+					Assembly assembly;
+					object robotClass;
+
+					//---- try to load the assembly
+					if (AssemblyManager.TryLoadAssembly(configuration.RobotClassAssemblyPath, out loadMessage, out assembly))
+					{
+						//---- try and load the class
+						if (AssemblyManager.TryLoadClass(assembly, configuration.RobotClassName, out loadMessage, out robotClass))
+						{
+							//---- check to see if it implements IRobot
+							if (robotClass is IRobot)
+							{
+								//---- cast
+								robot = robotClass as IRobot;
+							}
+							else //---- throw an exception
+							{ throw new RobotLoadFailedException("Robot load failed, custom class does not implement IRobot"); }
+						}
+						else //---- throw an exception
+						{ throw new RobotLoadFailedException("Robot load failed, custom class could not be loaded.", new Exception(loadMessage)); }
+					}
+					else //---- throw an exception
+					{ throw new RobotLoadFailedException("Robot load failed, custom class assembly could not be loaded.", new Exception(loadMessage)); }
+				}
+				else //---- throw an exception
+				{ throw new RobotLoadFailedException("Robot load failed, custom class assembly not found."); }
+			}
+
+			//---- if it has a custom UI
+			if (configuration.HasCustomUI)
+			{
+				//---- check to see if the assembly exists
+				if (System.IO.File.Exists(configuration.UIAssemblyPath))
+				{
+					//---- declare vars
+					Assembly assembly;
+					object uiClass;
+
+					//---- try to load the assembly
+					if (AssemblyManager.TryLoadAssembly(configuration.UIAssemblyPath, out loadMessage, out assembly))
+					{
+						//---- try and load the class
+						if (AssemblyManager.TryLoadClass(assembly, configuration.UIInitialClassName, out loadMessage, out uiClass))
+						{
+							//---- check to see if it's a UIElement
+							if (uiClass is System.Windows.UIElement) { }
+							else //---- throw an exception
+							{ throw new RobotLoadFailedException("Robot load failed, custom UI class must derive from UIElement"); }
+						}
+						else //---- throw an exception
+						{ throw new RobotLoadFailedException("Robot load failed, custom UI class could not be loaded.", new Exception(loadMessage)); }
+					}
+					else //---- throw an exception
+					{ throw new RobotLoadFailedException("Robot load failed, custom UI class assembly could not be loaded.", new Exception(loadMessage)); }
+				}
+				else //---- throw an exception
+				{ throw new RobotLoadFailedException("Robot load failed, custom UI class assembly not found."); }
+			}
+
+			//---- copy the configuration over
+			robot.Configuration = configuration;
+
+			//---- if we got here, all is well
+			return robot;
+		}
+		//=========================================================================
+	}
+}
